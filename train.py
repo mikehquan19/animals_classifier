4eb4b38ceaad07293d2c33b78a5b73b023f751d0
mikehquan19 <hoangphucquan19@gmail.com>
2025-08-13 01:37:41 -0500

    Add cosineannealingrestarts

--- train.py
+++ train.py
@@ -2,25 +2,28 @@ import torch
 from torch import cuda, optim, nn
 from torch.utils.data import DataLoader
 from dataset import AnimalImages
-from model.resnet import ResNet101Classifier
+from model.resnet import ResNet50Classifier
 
 @torch.no_grad()
-def get_accuracy(arg_model: nn.Module, data_loader: DataLoader, arg_device: cuda.device) -> float:
-    """ Calculate the accuracy of the model given the data loader """
+def get_accuracy(arg_model, data_loader, arg_device) -> float:
+    """ 
+    Calculate the accuracy of the model given the data loader 
+    """
 
     arg_model.eval() # Changing the model to eval mode
-    correct_prediction, total_prediction = 0, 0
+    correct_predictions, total_predictions = 0, 0
     for imgs, labels in data_loader:
         imgs, labels = imgs.to(arg_device), labels.to(arg_device)
         outputs = arg_model(imgs)
 
         # predicted label is one with largest probability, hence finding max
         _, predicted_labels = torch.max(outputs, dim=1)
-        # increment the number of correct predictions 
-        total_prediction += labels.shape[0]
-        correct_prediction += int((predicted_labels == labels).sum())
 
-    return round((correct_prediction / total_prediction) * 100, 2)
+        # increment the number of correct predictions 
+        total_predictions += labels.shape[0]
+        correct_predictions += int((predicted_labels == labels).sum())
+
+    return round((correct_predictions / total_predictions) * 100, 2)
 
 
 def mini_batch_training(num_epochs, model, loss_fn, optimizer, scheduler, train_data_loader, val_data_loader):
@@ -35,10 +38,9 @@ def mini_batch_training(num_epochs, model, loss_fn, optimizer, scheduler, train_
 
         # compute the train loss
         model.train() # switch the model to train mode
-        for imgs, labels in train_data_loader:
+        for batch_ix, (imgs, labels) in enumerate(train_data_loader):
             # make sure the imgs and the labels are converted to be used by right device 
             imgs, labels = imgs.to(device), labels.to(device)
-
             # forward phase
             predicted_train_labels = model(imgs)
             train_loss = loss_fn(predicted_train_labels, labels)
@@ -47,13 +49,15 @@ def mini_batch_training(num_epochs, model, loss_fn, optimizer, scheduler, train_
             optimizer.zero_grad()
             train_loss.backward() # calculate and accumulate the current gradient
             optimizer.step()
+
             avg_train_loss += train_loss.item()
+            if scheduler: 
+                # The user has an option of not using scheduler to train 
+                scheduler.step(epoch + batch_ix / len(train_data_loader)) # updates the LR every batch
     
         # compute train loss 
         avg_train_loss = round(avg_train_loss / len(train_data_loader), 4)
         train_losses.append(avg_train_loss)
-
-        scheduler.step() # Update the LR with the scheduler.
 
         # compute the val loss, turn off the autograd 
         with torch.no_grad():
@@ -77,40 +81,51 @@ def mini_batch_training(num_epochs, model, loss_fn, optimizer, scheduler, train_
 
 
 if __name__ == "__main__":
+    """ This current hyper-parameters achieved 80.7% -> 81.2% val accuracy """
     # Hyper-parameters
     LEARNING_RATE = 1e-4
     WEIGTH_DECAY = 1e-4
-    TOTAL_NUM_EPOCHS = 120 # adjust as needed, recommended as high as 100
-    NUM_EPOCHS_EACH_TIME = 50
-    BATCH_SIZE = 64 # adjust as needed 
+    TOTAL_NUM_EPOCHS = 200 # adjust as needed, recommended as high as 200
+    NUM_EPOCHS_EACH_CYCLE = 30
+    BATCH_SIZE = 128 # adjust as needed 
+    FIRST_ITERATION = True # used in case you resume training
 
     # initialize the dataset 
     train_dataset = AnimalImages("/content/animals10/raw-img", 224)
     val_dataset = AnimalImages("/content/animals10/raw-img", 224, train=False)
+    assert len(train_dataset) + len(val_dataset) == 26179
 
     # data loader of the dataset
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
     # initialize the model 
-    animal_classifier = ResNet101Classifier().to(
+    animal_classifier = ResNet50Classifier().to(
         torch.device("cuda") if cuda.is_available() else torch.device("cpu"))
     
-    # use ADAM to auto update the learning rate
+    # use ADAMW to auto update the learning rate
     this_optimizer = optim.AdamW(animal_classifier.parameters(), lr=LEARNING_RATE, weight_decay=WEIGTH_DECAY)
-    this_scheduler = optim.lr_scheduler.CosineAnnealingLR(this_optimizer, T_max=TOTAL_NUM_EPOCHS)
 
-    # Load the weights as well as state of model and optimizer and scheduler 
-    checkpoint = torch.load('./animals_checkpoint.tph')
+    # Scheduler to scheduler the global learning rate periodically
+    this_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        this_optimizer,
+        T_0=NUM_EPOCHS_EACH_CYCLE, # every cycle is 30 epochs, the scheduler restarts lr
+        T_mult=1, # cycle length the same
+        eta_min=1e-5 # min LR
+    )
 
-    animal_classifier.load_state_dict(checkpoint['model'])
-    this_optimizer.load_state_dict(checkpoint['optimizer'])
-    this_scheduler.load_state_dict(checkpoint['scheduler'])
+    # Load the weights as well as state of model and optimizer and schedulers
+    if not FIRST_ITERATION: 
+        checkpoint = torch.load('./data/animals_checkpoint.tph')
+
+        animal_classifier.load_state_dict(checkpoint['model'])
+        this_optimizer.load_state_dict(checkpoint['optimizer'])
+        this_scheduler.load_state_dict(checkpoint['scheduler'])
 
     # train the model
     torch.cuda.empty_cache() 
     train_track, val_track = mini_batch_training(
-        num_epochs=NUM_EPOCHS_EACH_TIME, 
+        num_epochs=TOTAL_NUM_EPOCHS, 
         model=animal_classifier,
         loss_fn=nn.CrossEntropyLoss(), 
         optimizer=this_optimizer,
@@ -120,11 +135,12 @@ if __name__ == "__main__":
     )
 
     # You can do something with train_track & val_track, like graphing them
-    # Optional, checking the accuracy 
+    torch.cuda.empty_cache()
     print(get_accuracy(animal_classifier, train_loader), get_accuracy(animal_classifier, val_loader))
+
     # save the model's weight to the file 
     torch.save({
         "model": animal_classifier.state_dict(),
         "optimizer": this_optimizer.state_dict(),
         "scheduler": this_scheduler.state_dict()
-    }, './animals_checkpoint.tph')
+    }, './data/animals_checkpoint.tph')
