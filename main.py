0a0cce7b744fbaf7964c4d9a827f731cf7f52c0d
mikehquan19 <hoangphucquan19@gmail.com>
2025-08-14 13:53:39 -0500

    Refactor the endpoints

--- main.py
+++ main.py
@@ -18,52 +18,52 @@ animals_classifier.load_state_dict(
     torch.load('./data/animals_checkpoint.pth', map_location=device)["model"])
 
 class UploadRequest(BaseModel): 
-    arg_url: str
+    image_url: str
 
-@app.post("/upload_url")
+@app.post("/upload_image_url")
 async def upload_url(request: UploadRequest): 
     """ Upload the image through the URL then predict the animal """
     try: 
-        response = requests.get(request.arg_url)
+        response = requests.get(request.image_url)
         image = Image.open(io.BytesIO(response.content))
     except:
         raise HTTPException(400, { 
             "status": 400, 
-            "message": "Image can't be opened because of invalid URL."
+            "message": "Image can't be opened, possibly because of invalid URL."
         })
     animal_prediction = predict(animals_classifier, image)
 
     return {
         "status": 200, 
         "data": {
-            "image_url": request.arg_url,
+            "image_url": request.image_url,
             "format": image.format,
             "result": animal_prediction
         }
     }
 
 
-@app.post("/upload_image")
-async def upload_image(arg_image: UploadFile): 
+@app.post("/upload_image_file")
+async def upload_image(image_file: UploadFile): 
     """ 
     Upload the image file then predict the animal. 
     Only files with PNG, JPG, JPEG, or WEBP extension are accepted.
     """
     try: 
-        image_content = await arg_image.read()
+        image_content = await image_file.read()
         image = Image.open(io.BytesIO(image_content))
     except: 
         raise HTTPException(400, { 
             "status": 400, 
-            "message": "Image can't be opened because of invalid file."
+            "message": "Image can't be opened, possibly because of invalid file."
         })
     animal_prediction = predict(animals_classifier, image)
     
     return {
         "status": 200, 
         "data": {
-            "filename": arg_image.filename,
-            "content_type": arg_image.content_type,
+            "filename": image_file.filename,
+            "content_type": image_file.content_type,
             "result": animal_prediction
         }
     }
