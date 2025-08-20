f2ec4ffeda420ceaca0ec8d1356469bd894ba538
mikehquan19 <hoangphucquan19@gmail.com>
2025-08-20 01:05:57 -0500

    Refactor for readability

--- model/efficientnet.py
+++ model/efficientnet.py
@@ -8,13 +8,17 @@ from configs import *
 class SEBlock(nn.Module): 
     """
     Architecture of squeeze and activation block (SE)
-    - Reading for [Squeeze and Excitation (SE)](https://towardsdatascience.com/introduction-to-squeeze-excitation-networks-f22ce3a43348/)
-    - Research paper of [SE](https://arxiv.org/pdf/1709.01507)
+        - Reading for [Squeeze and Excitation (SE)](https://towardsdatascience.com/introduction-to-squeeze-excitation-networks-f22ce3a43348/)
+        - Research paper of [SE](https://arxiv.org/pdf/1709.01507)
     ```
     expanded_features = num_features * scale
     reduced_features = num_features // reduction
     ```
-    Bottleneck from ```expanded_features``` to ```reduced_features```
+    Bottleneck from ```expanded_features``` to ```reduced_features```. 
+
+    It's also worth noting that excitation doesn't follow original architecture of the paper 
+    as it uses convolution layer instead of fully-connected layer.
+    It also uses Hardswish instead of Swish for activation function.
     """
 
     def __init__(self, num_features: int, scale: int, reduction: int) -> None: 
@@ -25,7 +29,7 @@ class SEBlock(nn.Module):
         # safeguard against out channels ~ 0 
         self.excitation = nn.Sequential(
             nn.Conv2d(num_features * scale, max(1, num_features // reduction), 1), 
-            nn.SiLU(),
+            nn.Hardswish(),
             nn.Conv2d(max(1, num_features // reduction), num_features * scale, 1),
             nn.Sigmoid()
         )
@@ -43,32 +47,38 @@ class MBConvBlock(nn.Module):
     - Image of the architecture of [MBConv](https://www.mdpi.com/computers/computers-13-00109/article_deploy/html/images/computers-13-00109-g002-550.jpg) (super convenient and illustrious)
 
     Args: 
-        scale (int): scaling the input channels over the layers in between (either 1 or 6)
-        in_channels (int): The features of the image tensor before feeding it through the model
+        scale (int): scaling the input channels over the layers in between 
+            (either 1 or 6)
+        in_channels (int): The features of the image tensor before 
+            feeding it through the model
         out_channels (int): The feature after the model
-        kernel_size (int): The size of the kernel for the depthwise convolutional block (either 3 or 5)
+        kernel_size (int): The size of the kernel for the depthwise convolutional block 
+            (either 3 or 5)
         stride (int): The stride for the depthwise convolutional block (either 1 or 2)
     """
     def __init__(
         self, scale: int, in_channels: int, out_channels: int, kernel_size: int, stride: int
     ) -> None: 
         super().__init__()
+
         if scale not in [1, 6]: 
             raise ValueError("Illegal scale, must be either 1 or 6.")
-        if kernel_size not in [3, 5]: 
+        elif kernel_size not in [3, 5]: 
             raise ValueError("Illegal kernel_size, must be either 3 or 5.")
-        if not 1 <= stride <= 2: 
+        elif not 1 <= stride <= 2: 
             raise ValueError("Illegal stride, must be either 1 or 2.")
 
+        # In MBConv, we only apply skip connection if we can directly without need of downsampling
         self.skip_connect = out_channels == in_channels and stride == 1
 
         # Pointwise convolution block to expand the channels (only for MBConv6)
-        self.pointwise_conv_with_activation= None
+        self.pointwise_conv_with_activation = None
         if scale == 6: 
             self.pointwise_conv_with_activation = nn.Sequential(
                 nn.Conv2d(in_channels, in_channels * scale, 1, bias=False),
                 nn.BatchNorm2d(in_channels * scale), 
-                nn.SiLU()) # Pytorch's swish activation
+                nn.SiLU() # Pytorch's swish activation
+            ) 
 
         # Depthwise convolution block
         self.depthwise_conv = nn.Sequential(
@@ -90,12 +100,12 @@ class MBConvBlock(nn.Module):
 
     def forward(self, x) -> Tensor: 
         if self.pointwise_conv_with_activation: 
-            output = self.depthwise_conv(self.pointwise_conv_with_activation(x))
+            output = self.pointwise_conv_with_activation(x)
+            output = self.depthwise_conv(output)
         else: 
             output = self.depthwise_conv(x)
 
         output = self.se(output)
-
         output = self.pointwise_conv_without_activation(output)
         if self.skip_connect: output = output + x
         return output
@@ -107,23 +117,12 @@ class EfficientNet(nn.Module):
 
     Args: 
         configs (Any): The configuration for the architecture of the model 
-            For example, 
-            ```
-            {
-                "conv3": {"out_channels": 32}, # the conf for the first convolution
-                "mbconv_blocks": [ # conf for the list of set of mbconv blocks 
-                    {"out_channels": 16, "kernel_size": 3, "stride": 1, "num_blocks": 1},
-                    ..., 
-                    {"out_channels": 100, "kernel_size": 1, "stride": 1, "num_blocks": 3},
-                ],
-                "conv1": {"out_channels": 1280} # the conf for the last convolution
-            }
-            ```
     """
 
     def __init__(self, configs: Any) -> None: 
         super().__init__()
         self.configs = configs
+
         # Non-properties, the initial input and output channels of the model 
         input_channels, output_channels = 3, self.configs["conv3"]["out_channels"]
 
@@ -140,12 +139,13 @@ class EfficientNet(nn.Module):
                 input_channels = output_channels 
                 # The output channels change for first block in every set of blocks 
                 if ix2 == 0: output_channels = config["out_channels"] 
+                # First set is MBConv1, other sets are MBConv6
+                this_scale = 1 if ix1 == 0 else 6 
+                # First block's stride follows the architecture, others' stride is 1
+                this_stride = config["stride"] if ix2 == 0 else 1 
 
                 mbConvBlocks.append(MBConvBlock(
-                    scale=1 if ix1 == 0 else 6, # first set is MBConv1, other sets are MBConv6
-                    in_channels=input_channels, out_channels=output_channels, 
-                    kernel_size=config["kernel_size"],
-                    stride=config["stride"] if ix2 == 0 else 1, # second to last block's stride = 1
+                    this_scale, input_channels, output_channels, config["kernel_size"], this_stride
                 ))
 
             setattr(self, f"mbconv{ix1}_blocks", nn.Sequential(*mbConvBlocks))
@@ -164,6 +164,6 @@ class EfficientNet(nn.Module):
         for ix in range(len(self.configs["mbconv_blocks"])):
             output = getattr(self, f"mbconv{ix}_blocks")(output)
 
-        output = self.pre_fc_block(output)
+        output = self.pre_fc_block(output) # (B, C, H, W) => (B, C, 1, 1)
         output = output.view(output.size(0), -1) # (B, C, 1, 1) => (B, C)
-        return self.fc(output)
+        return self.fc(output) # (B, C) => (B, 10)
